df9f46d6b6cd85b4d52fd6c936df0c17225b291f
Amir esfelurm <104654028+esfelurm@users.noreply.github.com>
2022-05-26 19:46:40 +0430

    Add files via upload

---
+++ Filtering.py
@@ -0,0 +1,268 @@
+ #import library's
+import socket
+server = socket.gethostname()
+ip=socket.gethostbyname(server)
+print ("server: {0}".format(server))
+print ("IP: {0}".format(ip))
+
+import os
+# auto install libs
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+os.system("pip install BeautifulTable")
+os.system("pkg install figlet")
+#importing libs
+from beautifultable import BeautifulTable
+import time                                          
+import sys                                                      
+import colored                                                  
+import webbrowser
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+import random
+from colorama import Fore, Back, Style                     
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+print(f"{rang} ")
+
+print (Fore.WHITE + "")
+
+#date
+
+
+
+print (Fore.YELLOW + "")
+K = """ [*] start Filtering....
+
+                                                       
+
+"""
+for c in K:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+
+print(f"{red} ")
+d = """
+
+
+_____ ____  _____ _____ _    _   _ ____  __  __
+| ____/ ___||  ___| ____| |  | | | |  _ \|  \/  |
+|  _| \___ \| |_  |  _| | |  | | | | |_) | |\/| |
+| |___ ___) |  _| | |___| |__| |_| |  _ <| |  | |
+|_____|____/|_|   |_____|_____\___/|_| \_\_|  |_|
+
+ESFELURM CHANNEL : @esfelurm  | Telegram & rubika 
+
+
+_____________________________________________
+
+,
+                     |'.             ,
+                     |  '-._        / )
+                   .'  .._  ',     /_'-,
+                  '   /  _'.'_\   /._)')
+                 :   /  '_' '_'  /  _.'
+                 |E |   |Q| |Q| /   /
+               .'  _\  '-' '-'    /
+              .'--.(S     ,__` )  /
+                    '-.     _.'  /
+                  __.--'----(   /
+              _.-'     :   __\ /
+             (      __.' :'  :Y
+              '.   '._,  :   :|
+                '.     ) :.__:|
+                  \    \______/
+                   '._D/_Z____]
+
+"""
+for c in d:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.005)
+print ("""
+esfelurm
+""")
+time.sleep(4.3)
+print (Fore.BLUE + "")
+time.sleep(1.7)
+print ("         Creator of this filtering script : Amir ")
+print ("                         ID telegram  &  Rubika : @esfelorm ")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("++++++++++++++++++++++++++++++++++++++++++++++")
+time.sleep(2.0)
+print ("             ♕      FILTERING       ♕    ")
+print ("")
+
+webbrowser.open("https://github.com/esfelurm")
+
+server = socket.gethostname()
+ip=socket.gethostbyname(server)
+print ("server: {0}".format(server))
+print ("IP: {0}".format(ip))
+
+print(f"{red} ")
+p = """"Please respect the copyright. This script is made by: esfelurm and any sale is followed by copyright.
+"""
+for c in p:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+print (Fore.GREEN +"")
+# input
+up = input("""
+
+_______________________________________________
+code channel [C] ^_^
+_______________________________________________
+code group [G] ^_^
+_______________________________________________
+code  account [A] ^_^
+_______________________________________________
+code makhlot [B]
+_______________________________________________
+Creative idea [T]
+_______________________________________________
+     number : 
+""")
+
+print(f"{pink} ")
+time.sleep(3.0)
+print("""
+ +________________________________________________________+
+""")
+
+print(f"{red} ")
+
+
+
+if up == "C" or up == "c":
+        print("☆")
+
+        print (Fore.GREEN +"")                  
+        print (" code filteri : ")
+        print (Fore.YELLOW + "") 
+
+po = ["http://10:38:02.000.ufhttk_6a56d.9062d.6f085.87ccchttps://www.pornhub.com/view_video.php?viewkey=ph61b3990115095(0, 0, 0, 0.2)(۳.۱.۶.۸/)rubika.fill-robika.fil.*(Af-aF-Af-aF/af+fil+by+rubika+*+*+", "  http://10:38:02.000.ufhttk_6a56d.9062d.6f085.87ccc40px.#f2f2f2_0 0 1px.)(0, 0, 0, 0.2)(۳.۱.۶.۸/)rubika.fill-robika.fil.*(Af-aF-Af-aF/af+fil+by+rubika+*+*+  ", " *https://ter.af-ter.af-ter.*0.84375rem 0.https://www.pornhub.com/view_video.php?viewkey=ph61b3990115095(/.40px.#f2f2f2_0 0 1px.)(0, 0, 0, 0.2)(۳.۱.۶.۸/)rubika.fill-robika.fil.*(Af-aF-Af-aF/af+fil+by+rubika+*+*+    ", "   http://10:38:02.000.ufhttk_6a56d.9062d.6f085.87ccchttps://www.pornhub.com/view_video.php?viewkey=ph61b3990115095 ", "    "]
+
+print(random.choice(po))
+
+
+if up != "C":
+        print("☆")
+
+
+if up == "G" or up == "g":
+             
+     print ('code for account ≠>' + '%s%s ((/19/:.21.38.6.98).7.1.7.6.33.3.6.1/(!))  ", "((/4.1.5.3/f////g.h/4.9.9.2.3.6.5.1.8.319.2888.7.1.9.3.4.1.1.3/!))", "((/5.2.7.1/f/h.u//4.8.2.7.8.3.2.8.1.2982.183.8.3/!))%s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "G":
+
+        print("☆")
+
+
+if up == "A" or up == "a":
+
+        print ('code for account ≠>' + '%s%s runtime-es2015.80.(Filter.Rubika.channel.esfe)0.3987.998cbc97194c7application/x-www-form-urlencoded %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up != "A":
+        print("☆")
+        
+if up == "B" or up == "b":
+        os.system("figlet MAKHLOT GAR")
+        print (Fore.GREEN +"")
+        os.system("clear") 
+        print("Enter your ip tor : ")
+        code = input()
+        print ("Link site sex : ")
+        sex = input()
+        print ("Link aks sexi : ")
+        aks = input()
+        print ("id target : \n(@esfelurm)")
+targete = input()
+print("http://1.7.2.5//e/(@supportbot)h//.",sex,"(iran-@support)0.5.4.8.9.sport.-rubika.-spam.(/yftt15k)_1.2.46kpd.9.hfdjsh.98.87.haY7.6.9.8.5.7.6//Fil//com.99999.4.7.1////",targete,".fil.(/yftt15k).sex.porn.huqhttps://s6.",aks,".www.pornhub.com(yftt13k)fil.*rubika.*fil_.rubika.M90.M21.M66.*M9999.(/yftt15k)./@support/f/1.1.1/2.2.2/3.3.",code,"///((/yftt99k)/*fill-*rubika*f*fill-xxx(Gif.sex)(*'*)/rest_122334_filteri.com.hsb_17g.sxs.www.com ")
+
+if up == "T":
+
+        print (' Creative idea   ≠>>>' + '%s%s rubika : Amir_esfelurm & telegram : esfelorm  %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "T":
+        print("☆")        
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+my channel : @esfelurm
+ """)
+print ("""
+link telegram & rubika : esfelurm
+""")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print ("by beybi  :)∆")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('baray filter kardan ziad gozaresh bazanid ta fil beshe ' + '%s%s  acant nabaiad bon bashe  %s' % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print (""",                                                               ,
+ '.                                                           .'/
+  ),\                                                         /,(
+ /__'.                                                     .'/__\
+ \  `'.'-.__                                           __.-'.'`  /
+  `)   `'-. \                                         / .-'`   ('
+  /   _.--'\ '.          ,               ,          .' /'--._   \
+  |-'`      '. '-.__    / \             / \    __.-' .'      `'-|
+  \         _.`'-.,_'-.|/\ \    _,_    / /\|.-'_,.-'`._         /
+   `\    .-'       /'-.|| \ |.-"   "-.| / ||.-'\       '-.    /`
+     )-'`        .'   :||  / -.\ //.- \  ||:   '.        `'-(
+    /          .'    / \_ |  /o`^'o\  | _// \    '.          \
+    \       .-'    .'   `--|  `"/ "`  |--`   '.    '-.       /
+     `)  _.'     .'    .--.; |\__"__/| ;.--.    '.     '._  ('
+     /_.'     .-'  _.-'     \ \/^\/ //     `-._  '-.     '._\
+     \     .'`_.--'          \     //          `--._`'.     /
+      '-._' /`            _   \-.-//   _            `\ '_.-'
+          `<     _,..--''`|    \`"`/    |`''--..,_     >`
+           _\  ``--..__   \     `'`     /   __..--``  /_
+          /  '-.__     ``'-;    / \    ;-'``     __.-'  \
+         |    _   ``''--..  '-' | '-'/  ..--''``   _    |
+         \     '-.       /   |/--|--\|   \       .-'     /
+          '-._    '-._  /    |---|---|    \  _.-'    _.-'
+              `'-._   '/ / / /---|---\ \ \ '   _.-'`
+                   '-./ / / / \`---`/ \ \ \ \.-'
+                       `)` `  /'---'\  ` `(`
+                 esf  /`     |       |     `\
+                     /  /  | |       | |  \  \
+                 .--'  /   | '.     .' |   \  '--.
+                /_____/|  / \._\   /_./ \  |\_____\
+               (/      (/'     \) (/     `\)      \)""")
+print(" ")
+print(" ")
+print(" ")
+time.sleep(2.0)
+webbrowser.open("https://t.me/esfelurm")
+
+table = BeautifulTable()
+table.rows.append(["AMIR.ESFELURM", "☆", "☆"])
+table.rows.append(["channel", "Telegram", "@esfelurm"])
+table.rows.append(["channel", "Rubika", "@PY_FELURM"])
+table.rows.header = ["1", "2", "3"]
+table.columns.header = ["name", "channel", "id channel "]
+print(table)
+print (Fore.WHITE + "")
